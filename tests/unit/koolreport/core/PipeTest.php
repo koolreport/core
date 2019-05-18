5b84c0036a83558ba77b8a6db6def4250f684c03
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2019-05-18 17:58:39 +0700

    Add the Pipe unit test

---
+++ tests/unit/koolreport/core/PipeTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace koolreport\core;
+
+use \koolreport\processes\Limit;
+use \koolreport\processes\Group;
+use \koolreport\processes\Sort;
+use \koolreport\core\Utility;
+
+class PipeTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    // tests
+    public function testPipeIf()
+    {
+        $start = new Limit(array(0));
+        
+        $node = $start->pipeIf(
+            true,
+            function ($n) {
+                return $n->pipe(new Group(array()));
+            },
+            function ($n) {
+                return $n->pipe(new Sort(array()));
+            }
+        );
+        $this->assertEquals(Utility::getClassName($node), "Group");
+
+
+        $node = $start->pipeIf(
+            false,
+            function ($n) {
+                return $n->pipe(new Group(array()));
+            },
+            function ($n) {
+                return $n->pipe(new Sort(array()));
+            }
+        );
+        $this->assertEquals(Utility::getClassName($node), "Sort");
+
+
+        $node = $start->pipeIf(
+            false,
+            function ($n) {
+                return $n->pipe(new Group(array()));
+            }
+        );
+        $this->assertEquals(Utility::getClassName($node), "Limit");
+
+    }
+}

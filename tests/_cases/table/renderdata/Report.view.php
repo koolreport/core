d5bd923464482440eecb256890fbdcf41a98b506
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2019-05-22 11:13:06 +0700

    Add the testing env for table

---
+++ tests/_cases/table/renderdata/Report.view.php
@@ -0,0 +1,16 @@
+<?php
+    use \koolreport\widgets\koolphp\Table;
+?>
+<html>
+    <head>
+        <title>Test Rendering Data At Client</title>
+    </head>
+    <body>
+        <h1>Test Rendering Data At Client</h1>
+        <?php
+        Table::create([
+            "dataSource"=>$this->src("automaker")->query("select * from customers")
+        ]);
+        ?>
+    </body>
+</html>

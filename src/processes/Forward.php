03872222232a76a5387fe3a39799bc1032b3b73e
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2021-09-22 17:05:14 +0700

    Adding process Forward

---
+++ src/processes/Forward.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file contains process to forward data only
+ *
+ * @category  Core
+ * @package   KoolReport
+ * @author    KoolPHP Inc <support@koolphp.net>
+ * @copyright 2017-2028 KoolPHP Inc
+ * @license   MIT License https://www.koolreport.com/license#mit-license
+ * @link      https://www.koolphp.net
+ */
+
+ namespace koolreport\processes;
+
+use \koolreport\core\Process;
+
+class Forward extends Process
+{
+    
+}
